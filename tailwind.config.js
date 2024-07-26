02acb448742a8efdfda463d85f159fab72e521ec
Foridul22 <foridulweb@gmail.com>
2024-07-26 15:43:02 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      Inter: "'Inter', sans-serif",
+      Poppins: "'Poppins', sans-serif",
+      Prata: "'Prata', serif",
+      Prompt: "'Prompt', sans-serif",
+    },
+  },
+  plugins: [],
+};
